044f6b50d6ef791f609d4797ee866452fd2a020c
Administrator <Administrator@MS-20170113ZGMB>
2018-11-27 15:15:52 +0800

    1

---
+++ sjavatools/src/main/java/com/ks/utils/webservice/SUtilWs.java
@@ -0,0 +1,30 @@
+package com.ks.utils.webservice;
+
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+
+/**
+ * @author pks
+ * @version 2018年11月27日
+ * 
+ * 	<dependency>
+		<groupId>org.apache.cxf</groupId>
+		<artifactId>cxf-rt-frontend-jaxws</artifactId>
+		<version>2.7.6</version>
+	</dependency>
+	<dependency>
+		<groupId>org.apache.cxf</groupId>
+		<artifactId>cxf-rt-transports-http</artifactId>
+		<version>2.7.6</version>
+	</dependency>
+ * 
+ */
+public class SUtilWs {
+	public static void main(String[] args) {
+//		JaxWsProxyFactoryBean fac = new JaxWsProxyFactoryBean();
+//		fac.setServiceClass(CnsServices.class);
+//		fac.setAddress("http://10.131.1.11:8080/CnsServices?wsdl");
+//		final CnsServices cs = (CnsServices) fac.create();
+//		String s = cs.getMac("3.64.27.66");
+	}
+}
+
